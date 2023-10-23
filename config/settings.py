3e9d027e944272e69043cbb8c134176edf8113fc
aryo <aryofaizi@gmail.com>
2023-10-23 17:47:16 +0330

    postgres database initialized

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-import environ
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,13 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-env = environ.Env()
-env.read_env()
 
-SECRET_KEY = env.read_env("DJANGO_SECRET_KEY")
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool("DJANGO_DEBUG")
+# DEBUG = config("DJANGO_DEBUG", cast=bool)
+DEBUG = config("debug")
 
 ALLOWED_HOSTS = []
 
@@ -79,8 +78,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config("docker_django_db"),
+        'USER': config("docker_django_db_user"),
+        'PASSWORD':config("docker_django_db_password"),
+        'HOST': 'db',
+        'PORT':5432,
     }
 }
 
