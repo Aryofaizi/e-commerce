05d3446a30b953b6881f959612074de02d513a6f
aryo <aryofaizi@gmail.com>
2023-10-23 17:54:45 +0330

    CustomUserAdmin created

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import CustomUser
 
-# Register your models here.
+1
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ["username", "email", "is_superuser"]
